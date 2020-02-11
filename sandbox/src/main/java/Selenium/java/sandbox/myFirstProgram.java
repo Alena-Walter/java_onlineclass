0b48a61375ed36577ae4941bfcc258d1747b5f3a
Alena-Walter <dorial@yandex.ru>
2020-02-10 20:00:01 -0500

    Functions are added

--- sandbox/src/main/java/Selenium/java/sandbox/myFirstProgram.java
+++ sandbox/src/main/java/Selenium/java/sandbox/myFirstProgram.java
@@ -2,7 +2,30 @@ package Selenium.java.sandbox;
 
 public class myFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        hello("Lena");
+        hello("User");
+
+        double l = 5.0;
+        System.out.println("square area with side " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 5;
+        System.out.println("rectangle area with sides " +a+ " and " +b+ " = " + area(a,b));
     }
+
+
+   public static void hello(String somebody) {
+        System.out.println("Hello " + somebody);
+    }
+
+    public static double area (double len) {
+        return len * len;
+    }
+
+    public static double area (double a, double b) {
+        return a*b;
+        }
+
+
 
 }
