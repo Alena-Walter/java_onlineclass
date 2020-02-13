76dcf614f58d9e5d6495d2a3f8f4562bba997e16
Alena-Walter <dorial@yandex.ru>
2020-02-12 20:39:02 -0500

    Repository update

---
+++ sandbox/src/test/java/ru/java/sandbox/MyFirstTest.java
@@ -0,0 +1,14 @@
+package ru.java.sandbox;
+
+import distance.Point;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MyFirstTest {
+
+    @Test
+    public void testDistance(){
+        Point p = new Point(2,6,4,8);
+        Assert.assertEquals(p.distance(), 5.656854249492381);
+    }
+}
