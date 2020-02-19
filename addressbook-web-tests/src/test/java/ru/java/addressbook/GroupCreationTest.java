2f21c095501b8bac97cdeef007b9671114efd61e
Alena-Walter <dorial@yandex.ru>
2020-02-18 20:51:29 -0500

    LoginFixtureAdded

--- addressbook-web-tests/src/test/java/ru/java/addressbook/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/java/addressbook/GroupCreationTest.java
@@ -14,10 +14,6 @@ public class GroupCreationTest {
     wd = new FirefoxDriver();
 
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void testGroupCreation() throws Exception {
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
@@ -27,6 +23,11 @@ public class GroupCreationTest {
     wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testGroupCreation() throws Exception {
+    
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
