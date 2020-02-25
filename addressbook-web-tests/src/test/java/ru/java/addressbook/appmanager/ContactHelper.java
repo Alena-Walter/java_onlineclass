af5926cea2094220b91fc6089daf8b91de85514b
Alena-Walter <dorial@yandex.ru>
2020-02-24 20:22:20 -0500

    Methods TYPE and CLICK for Contact Helper
    fully done

--- addressbook-web-tests/src/test/java/ru/java/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/java/addressbook/appmanager/ContactHelper.java
@@ -7,12 +7,13 @@ import ru.java.addressbook.model.GroupContact;
 
 import static org.testng.Assert.assertTrue;
 
-public class ContactHelper {
-    private WebDriver wd;
+public class ContactHelper extends HelperBase{
+
     public boolean acceptNextAlert = true;
 
     public ContactHelper(WebDriver wd) {
-        this.wd = wd;
+
+        super(wd);
     }
 
     public void acceptNextAlert() {
@@ -24,36 +25,27 @@ public class ContactHelper {
     }
 
     public void deleteContact() {
-      wd.findElement(By.xpath("//input[@value='Delete']")).click();
+
+        click(By.xpath("//input[@value='Delete']"));
     }
 
     public void selectContact() {
-        wd.findElement(By.name("selected[]")).click();
+        click(By.name("selected[]"));
 
     }
 
     public void submitContactForm() {
-      wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+
+        click(By.xpath("(//input[@name='submit'])[2]"));
     }
 
     public void fillContactForm(GroupContact groupContact) {
-      wd.findElement(By.name("firstname")).click();
-      wd.findElement(By.name("firstname")).clear();
-      wd.findElement(By.name("firstname")).sendKeys(groupContact.getFirstname());
-      wd.findElement(By.xpath("//div[@id='content']/form/label[3]")).click();
-      wd.findElement(By.name("lastname")).click();
-      wd.findElement(By.name("lastname")).clear();
-      wd.findElement(By.name("lastname")).sendKeys(groupContact.getLastname());
-      wd.findElement(By.name("theform")).click();
-      wd.findElement(By.name("address")).click();
-      wd.findElement(By.name("address")).clear();
-      wd.findElement(By.name("address")).sendKeys(groupContact.getAddress());
-      wd.findElement(By.name("mobile")).click();
-      wd.findElement(By.name("mobile")).clear();
-      wd.findElement(By.name("mobile")).sendKeys(groupContact.getMobile());
-      wd.findElement(By.name("email")).click();
-      wd.findElement(By.name("email")).clear();
-      wd.findElement(By.name("email")).sendKeys(groupContact.getEmail());
+        type(By.name("firstname"), groupContact.getFirstname());
+        type(By.name("lastname"), groupContact.getLastname());
+        type(By.name("address"), groupContact.getAddress());
+        type(By.name("mobile"), groupContact.getMobile());
+        type(By.name("email"), groupContact.getEmail());
+
     }
 
     public String closeAlertAndGetItsText() {
