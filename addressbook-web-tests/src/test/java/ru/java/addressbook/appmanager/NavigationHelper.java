77ac0d07d3e732f235f727638fd45ce98ad5e87b
Alena-Walter <dorial@yandex.ru>
2020-02-24 20:10:19 -0500

    Method CLICK for Navigation Helper

--- addressbook-web-tests/src/test/java/ru/java/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/java/addressbook/appmanager/NavigationHelper.java
@@ -3,26 +3,28 @@ package ru.java.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class NavigationHelper {
-    public WebDriver wd;
-
+public class NavigationHelper extends HelperBase{
     public NavigationHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
     }
 
     public void returntoGroupPage() {
-      wd.findElement(By.linkText("group page")).click();
+
+        click(By.linkText("group page"));
     }
 
     public void gotoGroups() {
-      wd.findElement(By.linkText("groups")).click();
+
+        click(By.linkText("groups"));
     }
 
     public void gotoHomePage() {
-        wd.findElement(By.linkText("home page")).click();
+
+        click(By.linkText("home page"));
     }
 
     public void gotoContactCreation() {
-      wd.findElement(By.linkText("add new")).click();
+
+        click(By.linkText("add new"));
     }
 }
