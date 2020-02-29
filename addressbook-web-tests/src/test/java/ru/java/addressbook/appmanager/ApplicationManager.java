da911ef26c2446117a34507e5a010e179bbce153
Alena-Walter <dorial@yandex.ru>
2020-02-29 14:21:42 -0500

    Parameter Browser Created
    3.3. done for FireFox, Chrome, IE

--- addressbook-web-tests/src/test/java/ru/java/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/java/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,10 @@
 package ru.java.addressbook.appmanager;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,9 +15,21 @@ public class ApplicationManager {
     public ContactHelper contactHelper;
     public GroupHelper groupHelper;
     public StringBuffer verificationErrors = new StringBuffer();
+    private String browser;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
 
     public void init() {
-        wd = new FirefoxDriver();
+
+        if (browser == BrowserType.FIREFOX) {
+            wd = new FirefoxDriver();
+        } else if (browser == BrowserType.CHROME) {
+            wd = new ChromeDriver();
+        } else if (browser == BrowserType.IE) {
+            wd = new InternetExplorerDriver();
+        }
 
         wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
