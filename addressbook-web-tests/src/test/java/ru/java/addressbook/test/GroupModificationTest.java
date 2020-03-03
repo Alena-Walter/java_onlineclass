abf3710f5668d44de4bb0f9e4cf122740ef1dd32
Alena-Walter <dorial@yandex.ru>
2020-03-02 20:04:21 -0500

    GroupModificationTest with check if group exists
    3.10 for Group Modofication Test

--- addressbook-web-tests/src/test/java/ru/java/addressbook/test/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/java/addressbook/test/GroupModificationTest.java
@@ -8,6 +8,15 @@ public class GroupModificationTest extends TestBase {
     @Test
     public void testGroupModification () {
         app.getNavigationHelper().gotoGroups();
+
+        //check if no one group exists
+        if (! app.getGroupHelper().isThereAGroup()) {
+            // then create one
+            app.getGroupHelper().createGroup(new GroupData("Walter1", null, null));
+
+        }
+        
+        app.getNavigationHelper().gotoGroups();
         app.getGroupHelper().selectGroups();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("Elena1", "Walter", null));
