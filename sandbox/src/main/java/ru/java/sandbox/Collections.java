929d80414e6d10c99a84a211b7b34c0ed2ce1ace
Alena-Walter <dorial@yandex.ru>
2020-03-05 19:52:23 -0500

    Arrays.asList

--- sandbox/src/main/java/ru/java/sandbox/Collections.java
+++ sandbox/src/main/java/ru/java/sandbox/Collections.java
@@ -1,6 +1,7 @@
 package ru.java.sandbox;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Collections {
@@ -8,14 +9,22 @@ public class Collections {
        //String [] langs = new String[4];
         String[] langs = {"Java", "C#", "Python", "PHP"};
 
-        //List, add value to List
-        List<String> languages = new ArrayList<String>();
-        languages.add("Java");
-        languages.add("C#");
-        languages.add("Python");
+        //List Option 1, add value to List
+        //List<String> languages = new ArrayList<String>();
+        //languages.add("Java");
+        //languages.add("C#");
+        //languages.add("Python");
 
-        for (String l : languages) {
-            System.out.println("I want to learn " + l);
+        //for (String l : languages) {
+            //System.out.println("I want to learn " + l);
+
+         //List Option 2 with special class Arrays.asList
+
+         List<String> languages = Arrays.asList("Java", "Python", "PHP");
+            for (String l : languages) {
+                System.out.println("I want to learn " + l);
+
+                
 
         // Perebor Massiva Option 1: Start with index 0, till last one, with step 1
        // for (int i = 0; i < langs.length; i++) {
