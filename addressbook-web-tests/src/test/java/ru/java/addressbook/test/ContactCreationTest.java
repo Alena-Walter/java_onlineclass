e50b5a49f0c77233e38f2e9793f98db4ca72668d
Alena-Walter <dorial@yandex.ru>
2020-03-05 20:40:49 -0500

    ContactCreationTest with size before_after
    4.3

--- addressbook-web-tests/src/test/java/ru/java/addressbook/test/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/java/addressbook/test/ContactCreationTest.java
@@ -1,5 +1,6 @@
 package ru.java.addressbook.test;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.java.addressbook.model.GroupContact;
 
@@ -9,10 +10,13 @@ public class ContactCreationTest extends TestBase {
   @Test
   public void testContactCreation() throws Exception {
 
+    int before = app.getContactHelper().getContactCount();
     app.getNavigationHelper().gotoContactCreation();
     app.getContactHelper().fillContactForm(new GroupContact("Elena1", "Walter", "Charlotte", "9262066973", null));
     app.getContactHelper().submitContactForm();
     app.getNavigationHelper().gotoHomePage();
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(after, before + 1);
   }
 
 
