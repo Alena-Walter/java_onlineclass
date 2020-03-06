12174c45861d147ccd0b3051f1589ccd39a573b1
Alena-Walter <dorial@yandex.ru>
2020-03-05 19:45:56 -0500

    List with 3 elements
    4.2 List till 10

--- sandbox/src/main/java/ru/java/sandbox/Collections.java
+++ sandbox/src/main/java/ru/java/sandbox/Collections.java
@@ -1,18 +1,30 @@
 package ru.java.sandbox;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Collections {
     public static void main (String[] arg) {
        //String [] langs = new String[4];
         String[] langs = {"Java", "C#", "Python", "PHP"};
 
-        // Perebor Option 1: Start with index 0, till last one, with step 1
+        //List, add value to List
+        List<String> languages = new ArrayList<String>();
+        languages.add("Java");
+        languages.add("C#");
+        languages.add("Python");
+
+        for (String l : languages) {
+            System.out.println("I want to learn " + l);
+
+        // Perebor Massiva Option 1: Start with index 0, till last one, with step 1
        // for (int i = 0; i < langs.length; i++) {
             //System.out.println("I want to learn " + langs[i]);
 
 
-            // Perebor Option 2: Start with index 0, till last one, with step 1
-            for (String l : langs) {
-                System.out.println("I want to learn " + l);
+            // Perebor Massiva Option 2: Start with index 0, till last one, with step 1
+            //for (String l : langs) {
+                //System.out.println("I want to learn " + l);
 
         }
     }
