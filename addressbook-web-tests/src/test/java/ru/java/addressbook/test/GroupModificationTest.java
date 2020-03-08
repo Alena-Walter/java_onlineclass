31641c3d129781ff2c86383f4131f5254f3235d7
Alena-Walter <dorial@yandex.ru>
2020-03-07 21:17:07 -0500

    4.7 GroupModificationTest first check added
    4.7 til 3-22

--- addressbook-web-tests/src/test/java/ru/java/addressbook/test/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/java/addressbook/test/GroupModificationTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.java.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTest extends TestBase {
@@ -24,11 +25,17 @@ public class GroupModificationTest extends TestBase {
         //int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().selectGroups(before.size() - 1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("Elena_for test", "I did", null));
+        GroupData group = new GroupData ("Elena_for test", "I did", null);
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getNavigationHelper().returntoGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         //int after = app.getGroupHelper().getGroupCount();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+
     }
 }
