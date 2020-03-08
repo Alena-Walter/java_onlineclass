bf971e348a26169318dfe23a3612430c212301bc
Alena-Walter <dorial@yandex.ru>
2020-03-07 19:24:31 -0500

    GroupDeletionTest with list size check
    4.5 for GroupDeletionTest

--- addressbook-web-tests/src/test/java/ru/java/addressbook/test/GroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/java/addressbook/test/GroupDeletionTest.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.java.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupDeletionTest extends TestBase {
 
   @Test
@@ -18,12 +20,14 @@ public class GroupDeletionTest extends TestBase {
 
     }
     app.getNavigationHelper().gotoGroups();
-    int before = app.getGroupHelper().getGroupCount();
-    app.getGroupHelper().selectGroups(0);
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    //int before = app.getGroupHelper().getGroupCount();
+    app.getGroupHelper().selectGroups(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups();
     app.getNavigationHelper().returntoGroupPage();
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before - 1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    //int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after.size(), before.size() - 1);
   }
 
 }
